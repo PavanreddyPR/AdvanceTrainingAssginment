9aa73a7bdb5c5e49d56f56cb624e3d0568c0a7cf
PavanreddyPR <97774367+PavanreddyPR@users.noreply.github.com>
2022-03-21 14:17:55 +0530

    16
    Climbing Stairs

---
+++ ClimbingStairs.java
@@ -0,0 +1,26 @@
+package pack16;
+
+public class ClimbingStairs {
+
+	// A simple recursive program to find
+	// n'th fibonacci number
+	
+	static int fib(int n)
+	{
+		if (n <= 1)
+			return n;
+		return fib(n - 1) + fib(n - 2);
+	}
+
+	// Returns number of ways to reach s'th stair
+	static int countWays(int s)
+	{
+		return fib(s + 1);
+	}
+
+	public static void main(String args[])
+	{
+		int s = 3;
+		System.out.println("Number of ways = " + countWays(s));
+	}
+} 
